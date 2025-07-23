363f206ae660d252a99a79046376a9249fb59d0a
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-07-23 12:15:06 +0200

    Creation de la page panier

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/cart', [CartController::class, 'index']);
