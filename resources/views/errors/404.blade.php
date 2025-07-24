bc4d348b4eff675ea61a9131168f2a685f17a5d7
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-07-24 14:11:00 +0200

    Ajout page 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<x-head />
+
+<body>
+    <div class="container d-flex flex-column justify-content-center align-items-center" style="height: 100vh">
+        <div class="row" >
+            <div class="col">
+                <h1 class="text-center">La page 404 de Croc Pages est introuvable</h1>
+            </div>
+        </div>
+        <div class="row justify-content-center" >
+            <div class="col-6">
+                <img class="img-fluid" src="https://static.vecteezy.com/ti/vecteur-libre/p1/7518071-un-vieux-livre-ouvert-avec-une-page-dechiree-cartoon-vector-illustration-sur-un-fond-blanc-vectoriel.jpg" alt="Image d'un livre" />
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
