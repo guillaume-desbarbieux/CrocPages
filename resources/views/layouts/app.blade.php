8337e847e7455b36d1240d6eb70c9cf819c8a4ea
Emin Bahous <emin.bahous@le-campus-numerique.fr>
2025-07-24 10:46:15 +0200

    layout header footer nav

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <title>@yield('title', 'Croc-Page')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+
+   <header>
+    <nav>
+        <a href="{{ route('home') }}">Accueil</a> |
+        <a href="{{ route('catalog') }}">Catalogue</a>
+    </nav>
+</header>
+
+
+  <div class="container mt-4">
+        @yield('content')
+    </div>
+
+    <footer>
+       footer
+    </footer>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CatalogController;
 
 Route::get('/', [HomeController::class, 'index'])->name("homepage");
 
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
+
+Route::get('/Catalog', [CatalogController::class, 'index']) ->name('catalog');
