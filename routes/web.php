4ab25379b48dca809acfe4b79de0f6ebe95275ee
Geraud <geraud-yakou.clement@le-campus-numerique.fr>
2025-07-24 11:20:21 +0200

    Revert "Creation page panier"

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
-use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CatalogController;
 
-Route::get('/', [HomeController::class, 'index'])->name("homepage");
-
-Route::get('/cart', [CartController::class, 'index'])->name('cart');
-
-Route::get('/Catalog', [CatalogController::class, 'index']) ->name('catalog');
+Route::get('/', [HomeController::class, 'index']);
