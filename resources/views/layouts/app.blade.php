697bdad469670c627c2f162d2b0036cdea5f987d
ElricTOURTEL <tourtel.elric@gmail.com>
2025-07-24 16:29:04 +0200

    feat(header): modification style du header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <title>@yield('title', 'Mon site')</title>
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <style>
+    .gradient-header {
+        background: linear-gradient(to bottom, #f7e89b 0%, #ffffff 100%);
+    }
+    </style>
 </head>
 <body>
 <header>
