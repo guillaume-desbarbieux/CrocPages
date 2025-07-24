77342f30459a19b6c74cb3fa48d6f4d62292e5df
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-07-24 12:03:05 +0200

    Feat link to product.show from homepage

--- resources/views/components/home/card.blade.php
+++ resources/views/components/home/card.blade.php
@@ -1,20 +1,17 @@
 @props(['article'])
+    <a class="row h-95" href= {{  route('catalog.show', ['id'=>$article['id']]) }}>
+        <img class="img-fluid" src="{{ $article['imgUrl'] }}"
+            alt="image de couverture de {{ $article['title'] }} ({{ $article['author'] }})">
+    </a>
 
-<div class="row h-95">
-    <img class="img-fluid"
-    src="{{ $article['imgUrl'] }}"
-    alt="image de couverture de {{ $article['title'] }} ({{ $article['author'] }})"
-    >
-</div>
+    <div class="row">
+        <h3>Titre : {{ $article['title'] }} </h3>
+    </div>
 
-<div class="row">
-    <h3>Titre : {{ $article['title'] }} </h3>
-</div>
+    <div class="row">
+        <p>Auteur : {{ $article['author'] }}</p>
+    </div>
 
-<div class="row">
-    <p>Auteur : {{ $article['author'] }}</p>
-</div>
-
-<div class="row">
-    <p>Prix : {{ $article['price'] }} €</p>
-</div>
+    <div class="row">
+        <p>Prix : {{ $article['price'] }} €</p>
+    </div>
