4912e43bcc16829090dd2774beea6060918c5b7c
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-07-24 11:16:24 +0200

    Ajout du total panier

--- resources/views/cart/total-cart.blade.php
+++ resources/views/cart/total-cart.blade.php
@@ -1,10 +1,10 @@
-<div class="container">
+<div class="container shadow border rounded m-4 p-2">
     <div class="row">
         <div class="col-6">
             <p>Produits</p>
         </div>
         <div class="col-6">
-            <p>249.99€</p>
+            <p>{{ $totalPrice }}€</p>
         </div>
     </div>
     <div class="row">
@@ -12,7 +12,7 @@
             <p>Livraison</p>
         </div>
         <div class="col-6">
-            <p>10.50€</p>
+            <p>{{ $shippingPrice }}€</p>
         </div>
     <div class="row">
         <div class="col-6">
