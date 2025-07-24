953e64cd954e8f3b4a170129747ae3c83e1fd31c
ElricTOURTEL <tourtel.elric@gmail.com>
2025-07-24 13:51:45 +0200

    feat(refactor): Mise en place du header / Searchbar et promo bar

--- resources/views/components/promo-banner.blade.php
+++ resources/views/components/promo-banner.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-danger" style="height:50px;">
+<div class="bg-danger" style="height:25px;">
     <div class="row h-100">
         <div class="justify-content-center col d-flex align-items-center">
             <p class="text-warning text-center text-wrap m-0">
