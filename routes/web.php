526af785966fcbb31c10c68a912fa9241304517b
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-07-25 16:55:54 +0200

    Affichage des éléments de la base de donnée

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CatalogController;
 
 Route::get('/', [HomeController::class, 'index'])->name("homepage");
-
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
-
 Route::get('/Catalog', [CatalogController::class, 'index']) ->name('catalog');
 Route::get('/product/{id}', [ProductController::class, 'show'])->name('catalog.show');
 
