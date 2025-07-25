fe6f99a34201f4abc230bf18fbb6e6c49a3a9d46
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-07-25 09:17:45 +0200

    Feat(homepage) : Add picture of library

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <h1>Ma boutique</h1>
+    <p> photo de ma boutique </p>
+</div>
+
+
     <div class="row mx-0 my-2 text-center bg-warning-subtle ">
         <h2>Meilleures Ventes</h2>
     </div>
