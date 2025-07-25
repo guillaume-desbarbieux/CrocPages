15fc1ed65c9e8a6e2d7785bb36fbe266fe832786
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-07-25 16:32:00 +0200

    passing dt to controllers and views

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\product;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\DB;
+
+use function Laravel\Prompts\select;
 
 class HomeController extends Controller
 {
-    function index(): View{
-        $articles = include base_path('data/articles.php');
-        return view('homepage', compact('articles'));
+    function index(): View
+    {
+        //$articles = DB::select('select * from products where category_id=1');
+
+        $products = product::where('category_id', "=",2 )->get();
+
+
+        return view('homepage', compact('products'));
     }
 }
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,20 +1,27 @@
-@props(['article'])
-<a class="row h-95" href={{  route('catalog.show', ['id' => $article['id']]) }}>
-    <img class="img-fluid object-fit-contain" style="height:350px" src="{{ $article['imgUrl'] }}"
-        alt="image de couverture de {{ $article['title'] }} ({{ $article['author'] }})">
+@props(['product'])
+
+{{--@dump($product)--}}
+
+<a class="row h-95" href={{  route('catalog.show', ['id' => $product->id]) }}>
+    <img class="img-fluid object-fit-contain" style="height:350px" src="{{ $product->imgURL }}"
+        alt="image de couverture de {{ $product->title }} ({{ $product->author }})">
 </a>
 
 
 <div class="row">
-    <h5 class="card-title">{{ $article['title'] }}</h5>
+    <h5 class="card-title">{{ $product->title }}</h5>
 </div>
 
 <div class="row">
-    <p class="card-text">Auteur : {{ $article['author'] }}</p>
+    <p class="card-text">Auteur : {{ $product->author }}</p>
 </div>
 
 <div class="row">
-    <p class=" col card-text">Prix : {{ $article['price'] }} €</p>
+    <p class="card-text">Catégorie : {{ $product->category->name }}</p>
+</div>
+
+<div class="row">
+    <p class=" col card-text">Prix : {{ $product->price }} €</p>
     <a class="position-absolute bottom-0 end-0 object-fit-content" href={{route('cart')}} style="width: 4em">
         <img class="img-fluid" src="{{ asset('images/logobasket.svg') }}" alt="logo panier">
     </a>
