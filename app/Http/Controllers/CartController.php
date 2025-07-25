9cce0ec0ca280bd4cb3a21deefcd3b0dc7d38a06
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-07-25 13:41:21 +0200

    Ajout exemple de requetes dans cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
     function index(): View{
-        return view('cart.cart-show');
+        $users = DB::select('select * from users');
+        @dump($users);
+        $cart = DB::select('select * from cart');
+        @dump($cart);
+
+        // DB::table('cart')->insertOrIgnore([
+        //     'user_id' => rand(1, 2), 
+        //     'product' => 'Sample Product',
+        //     'quantity' => rand(1, 5),
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+
+        return view('cart.cart-show', [
+            'cart' => $cart,
+        ]);
     }
 }
