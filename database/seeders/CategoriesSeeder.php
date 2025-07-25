fcfab86b737ab9ce164835c41a4741911a5feba0
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-07-25 16:18:22 +0200

    adding seeders for categories and products

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            ['name' => 'Science-Fiction'],
+            ['name' => 'Policier'],
+            ['name' => 'Documentaire']
+        ];
+
+        foreach ($categories as $category) {
+            DB::table('categories')->insert($category);
+        }
+    }
+}
