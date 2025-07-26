b1fddbe215cefd1570a695a577543bba57f54836
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-07-26 18:25:48 +0200

    Ajout des requetes a la base de données pour le panier

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use App\Models\CartItem;
+use App\Models\User;
 
 class CartController extends Controller
 {
@@ -13,11 +16,33 @@ class CartController extends Controller
 
         // $cart = DB::select('select * from cart');
         // @dump($cart);
+        // $cart = Cart::where('user_id', 1)->first()->id;
+        // @dump($cart);
+        // $cart = Cart::all();
+        // @dump($cart);
 
-        $products = Product::all();
+        //$products = Product::all();
+
+        // $cartOfUser = Cart::where('user_id', 2)->where('is_paid', false)->first()->id;
+        // $products = CartItem::where('cart_id', $cartOfUser);
+        // @dump($products);
+
+        $productsList = [];
+        $user = User::find(2);
+        $cart = $user->cart()->where('is_paid', false)->first();
+        //@dump($cart);
+
+        if($cart){
+            foreach($cart->items as $item){
+                $itemWithQuantity = $item->product;
+                $itemWithQuantity['quantity'] = $item->quantity;
+                array_push($productsList, $itemWithQuantity);
+            }
+        }
+        //@dump($productsList);
 
         return view('cart.cart-show', [
-            'productsList' => $products,
+            'productsList' => $productsList,
         ]);
     }
 }
--- resources/views/cart/cart-show.blade.php
+++ resources/views/cart/cart-show.blade.php
@@ -1,19 +1,26 @@
 @extends('cart.cart')
 
+@php
+    $totalPrice = 0;
+    $shippingPrice = 10.50;
+@endphp
 @section(section: 'products')
     @foreach ($productsList as $product)
         @include('cart.card',[
                     'title' => $product -> name,
                     'author' => $product -> author,
                     'price' => $product -> price,
-                    'quantity' => 1,
+                    'quantity' => $product -> quantity,
                     'urlImg' => $product -> img_url
-        ]) 
+        ])
+        @php
+            $totalPrice += $product -> price * $product -> quantity; 
+        @endphp
     @endforeach
 @endsection
 @section('total-cart')
     @include('cart.total-cart',[
-        'totalPrice' => 300.99,
-        'shippingPrice' => 10.50
+        'totalPrice' => $totalPrice + $shippingPrice,
+        'shippingPrice' => $shippingPrice,
     ])
 @endsection
