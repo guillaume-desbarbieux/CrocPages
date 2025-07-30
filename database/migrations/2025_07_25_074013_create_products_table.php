19db4dca2ffea847af2bf5da3119eb376ed7b3ac
ElricTOURTEL <tourtel.elric@gmail.com>
2025-07-30 16:28:30 +0200

    ajout pour travail maison

--- database/migrations/2025_07_25_074013_create_products_table.php
+++ database/migrations/2025_07_25_074013_create_products_table.php
@@ -20,12 +20,6 @@ return new class extends Migration
             $table->integer('price');
             $table->integer('stock')->default(10);
             $table->timestamps();
-            $table->unsignedBigInteger('tag_id');
-            $table->foreign('tag_id')
-                ->references('id')
-                ->on('tags')
-                ->onDelete('restrict')
-                ->onUpdate('restrict');
         });
     }
 
