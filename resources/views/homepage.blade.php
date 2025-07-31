80c7ae5d0e1f08ce3b4bf0be0694990948244e6b
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-07-31 16:45:48 +0200

    feat affichage bandeau no stock

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -81,11 +81,9 @@
 
         <div class="row justify-content-center mt-5">
             @foreach ($products as $product)
-                @if ($product->stock > 0)
                     <div class="card hover-card col-12 col-sm-5 col-md-5 col-lg-2 text-center mx-2 mb-3">
                         <x-card :product="$product" />
                     </div>
-                @endif
             @endforeach
         </div>
     </div>
