58b960f6fb916c03f476d0b331825dc145f76075
ElricTOURTEL <tourtel.elric@gmail.com>
2025-07-31 17:19:18 +0200

    feature: ajout des tags dans la recherche

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,7 +10,11 @@ class SearchController extends Controller
     public function search(Request $request)
     {
         $input = $request->input('query');
-        $products = Product::where('title', 'like', "%$input%")->orWhere('author', 'like', "%$input%")->get();
+        $products = Product::where('title', 'like', "%$input%")->orWhere('author', 'like', "%$input%")
+        ->orWhereHas('tags', function ($tagQuery) use ($input) {
+            $tagQuery->where('name', 'like', "%$input%");})->get();
+
+            // https://laravel.com/docs/12.x/eloquent-relationships#querying-relationship-existence Lien de la doc sur le wherehas
         return view('search.search', compact('products', 'input'));
     }
 }
