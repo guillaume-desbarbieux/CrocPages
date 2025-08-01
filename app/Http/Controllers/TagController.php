7be1b1c37e5ffae71ff76c0e272522edf5197dfd
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-08-01 16:08:46 +0200

    Merge branch 'dev' of github.com:guillaume-desbarbieux/CrocPages into dev

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:50|unique:tags,name',
+        ]);
+
+        $tag = Tag::create(['name' => $validated['name']]);
+
+        return redirect()->route('backoffice.product.create')
+                         ->with('success', 'Tag créé avec succès !');
+    }
+}
